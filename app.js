aa38f2126a3bfc02d9ac0a936dcd1563eb13a2a0
Justin Barabander <justinbarabander@Justins-MacBook-Pro.local>
2015-08-05 17:43:52 -0400

    Started Working on websocket functionality

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var swig = require('swig');
 var routes = require('./routes/');
-
+var socketio = require('socket.io');
 var app = express();
 
 app.engine('html', swig.renderFile);
@@ -26,10 +26,10 @@ swig.setDefaults({cache: false});
 
 app.use(morgan('dev'))
 
-app.use(routes);
-
 var server = app.listen(3000, function(){
   var host = server.address().address;
   var port = server.address().port;
   console.log('app is running at http://%s:%s', host, port);
 })
+var io = socketio.listen(server);
+app.use(routes(io));
--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,56 @@
 var express = require('express');
-var router = express.Router();
 var tweetBank = require('../tweetBank');
 var path = require('path');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
-router.use(bodyParser());
 
-router.use('/', function (req, res, next) {
-  console.log('PATH:',path.join(__dirname, '../public/' + req.path));
-  res.sendFile(path.join(__dirname, '../public/' + req.path), function(err) {
-    if(err) {
-      // console.log('err');
-      next();
-    }
+
+module.exports = function(io) {
+  var router = express.Router();
+  router.use(bodyParser());
+
+  router.use('/', function (req, res, next) {
+    console.log('PATH:',path.join(__dirname, '../public/' + req.path));
+    res.sendFile(path.join(__dirname, '../public/' + req.path), function(err) {
+      if(err) {
+        // console.log('err');
+        next();
+      }
+    })
   })
-})
 
-router.get('/user/:name/tweets/:id', function(req,res) {
-  var name = req.params.name;
-  var postStatus = '';
-  var id = parseInt(req.params.id);
-  var list = tweetBank.find({name: name, id: id});
-  if(!list.length){
-    postStatus = 'Could not find that post';
-  }
-  res.render('index', {title: 'Twitter.js Post by ' + name, tweets: list, notification: postStatus})
-})
+  router.get('/user/:name/tweets/:id', function(req,res) {
+    var name = req.params.name;
+    var postStatus = '';
+    var id = parseInt(req.params.id);
+    var list = tweetBank.find({name: name, id: id});
+    if(!list.length){
+      postStatus = 'Could not find that post';
+    }
+    res.render('index', {title: 'Twitter.js Post by ' + name, tweets: list, notification: postStatus})
+  })
 
-router.get('/user/:name', function(req, res) {
-  name = req.params.name;
-  var list = tweetBank.find({name: name})
-  res.render('index', {title: 'Twitter.js - Posts by ' + name, name: name, tweets: list, showForm: true})
-})
+  router.get('/user/:name', function(req, res) {
+    name = req.params.name;
+    var list = tweetBank.find({name: name})
+    res.render('index', {title: 'Twitter.js - Posts by ' + name, name: name, tweets: list, showForm: true})
+  })
 
-router.get('/', function (req, res) {
-  var name = '';
-  var tweets = tweetBank.list();
-  res.render('index', {title: 'Twitter.js', name: name, tweets: tweets, showForm: true})
-})
+  router.get('/', function (req, res) {
+    var name = '';
+    var tweets = tweetBank.list();
+    res.render('index', {title: 'Twitter.js', name: name, tweets: tweets, showForm: true})
+  })
 
-router.post('/submit', function(req,res) {
-  console.log(req.body)
-  var name = req.body.name;
-  var text = req.body.text;
-  // console.log(name,text);
-  tweetBank.add(name, text);
-  res.redirect('/');
-})
-
-module.exports = router;
+  router.post('/submit', function(req,res) {
+    console.log(req.body)
+    var name = req.body.name;
+    var text = req.body.text;
+    // console.log(name,text);
+    tweetBank.add(name, text);
+    io.sockets.emit('new_tweet', {name: name, text: text});
+    res.redirect('/');
+  })
+  return router;
+};
